e82ee8dacd08b3cb303a426767abad518974ad18
Seong Cho <seongcho9803@gmail.com>
2020-07-08 01:24:33 -0400

    create chapter1

---
+++ chapter1/chapter1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ *  This program is for chapter 1 of C++ CPA Certification
+ **/
+
+int main() {
+    int byte = 255;
+    std::cout << "Byte in hex: " << std::hex << byte;
+
+    return 0;
+}
--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
+/**
+ *  This is a sample program that I wrote in order to set up c++ extension in visual studio code
+ **/
 int main(){
     vector<string> msg {"Hello", "C++", "World", "from", "and the c++ extension!"};
 
+    int i = 0;
     for (const string& word : msg){
         cout << word << " ";
+        i++;
     }
     cout << endl;
 }
